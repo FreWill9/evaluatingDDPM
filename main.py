a63224a9a1957a24896b6eb3ca56bf4d26a4b4aa
Frederik Willger <frederik.willger@web.de>
2026-03-04 15:22:34 +0100

    Outline train-loop

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
+import torch.optim as optim
+from models.unet import UNet
 import numpy as np
 import random
 import math
 
 
-def train():
-    pass
+def train(batch_size: int,
+          num_epochs: int,
+          num_timesteps: int,
+          lr: float = 2e-5):
+    # TODO: use correct dataset
+    train_dataset = datasets.MNIST(root='./data', train=True, download=False, transform=transforms.ToTensor())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
+
+    model = UNet()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.MSELoss(reduction='mean')
+
+    # TODO: checkpoint
+
+    for i in range(num_epochs):
+        pass
+
+    # TODO: checkpoint
 
 
 def sample():
@@ -15,7 +35,7 @@ def sample():
 
 
 def main():
-    train()
+    train(batch_size=128, num_epochs=15, num_timesteps=1_000)
     # sample()
 
 
