5c891dfaa3a307215ecd65e90b0e01367c528ff2
Frederik Willger <frederik.willger@web.de>
2026-03-04 14:11:43 +0100

    Define structure

---
+++ main.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import random
+import math
+
+
+def train():
+    pass
+
+
+def sample():
+    pass
+
+
+def main():
+    train()
+    # sample()
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/unet.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class UNetLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class SinusoidalEmbeddings(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
