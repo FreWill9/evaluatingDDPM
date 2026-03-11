8c8c0a66ea902cea16c4f49203600e6b303d499b
ahenkel <anton.henke@stud.uni-heidelberg.de>
2026-03-11 12:52:23 +0100

    Save preprocessed CelebA to .pt

---
+++ save_preprocessed_celeba2.py
@@ -0,0 +1,29 @@
+from celeba_dataset import CelebAGray32
+from torch.utils.data import Subset
+import torch
+import os
+import random
+
+data_dir = r"D:/data/img_align_celeba"
+save_dir = "data"
+img_size = 32
+subset_size = 5000
+
+os.makedirs(save_dir, exist_ok=True)
+
+full_dataset = CelebAGray32(data_dir, img_size=img_size)
+
+indices = list(range(len(full_dataset)))
+random.shuffle(indices)
+indices = indices[:subset_size]
+dataset = Subset(full_dataset, indices)
+
+# Stack all tensors into one big tensor
+all_tensors = torch.stack([dataset[i] for i in range(len(dataset))])
+
+print(all_tensors.shape)
+print(all_tensors.dtype)
+
+# Save to disk
+torch.save(all_tensors, f"{save_dir}/celeba_gray32_{subset_size}.pt")
+print(f"Saved preprocessed CelebA subset to disk")
