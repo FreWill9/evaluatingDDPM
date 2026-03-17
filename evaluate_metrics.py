4a54db311c32140a935a78365ebdeb1a49790991
ahenkel <anton.henke@stud.uni-heidelberg.de>
2026-03-17 12:48:03 +0100

    Update the main block and added findings

--- evaluate_metrics.py
+++ evaluate_metrics.py
@@ -81,41 +81,53 @@ def compute_nn_distance(real_dir, gen_dir, subsample=None):
 
 if __name__ == "__main__":
 
-    save_path_real = r"D:/data/evaluation/real_images/celeba_32_5000"
-    save_path_generated = r"D:/data/evaluation/generated_images/simple_unet32_celeba5000"
+    # Select model architecture
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SimpleUnet().to(device)
+
+    # Set path to preprocessed dataset and model checkpoint
+    data_name = "digits_gray16_1797"
+    data_path = f"data/{data_name}.pt"
+
+    checkpoint_name = "simple_unet_5000eps_digits16"
+    checkpoint_path = f"checkpoints/{checkpoint_name}.pt"
+
+    # Set paths for real and generated images for evaluation
+    save_path_real = f"D:/data/evaluation/real_images/{data_name}"
+    save_path_generated = f"D:/data/evaluation/generated_images/{checkpoint_name}"
+
 
     # Step 1: Save preprocessed real images as PNGs for metric calculation
-    data_path = "data/celeba_gray32_5000.pt"
+
     if not os.path.exists(save_path_real) or len(os.listdir(save_path_real)) == 0:
         save_preprocessed_real_images(data_path, save_path_real)
+        print(f"Saved preprocessed real images for evaluation at: {save_path_real}")
     else: 
         print(f"Real images already exist at: {save_path_real}, skipping saving step.")
-        
-    
-    # Step 2: Generate and save images as PNGs from the trained model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Select model and load checkpoint
-    model = SimpleUnet().to(device)
-    checkpoint_path = "checkpoints/latest_simple_unet32.pt"
+    
+    # Step 2: Generate and save images as PNGs from the trained model for metric calculation
+
+    # Load checkpoint
     ckpt = torch.load(checkpoint_path, map_location=device)
     model.load_state_dict(ckpt["model_state_dict"])
     model.eval()
 
-    img_size = 32
-    betas = linear_beta_schedule(1000).to(device)
+    img_size = 16
+    num_timesteps = 1000
+    betas = linear_beta_schedule(num_timesteps).to(device)
     schedule = precompute_schedule(betas)
 
-    num_timesteps = 1000
     num_samples = len(os.listdir(save_path_real))  # Match number of real images
     if not os.path.exists(save_path_generated) or len(os.listdir(save_path_generated)) == 0:
+        print("Generating and saving generated images for evaluation...")
         save_images(model, schedule, device, img_size, num_timesteps=num_timesteps, num_samples=num_samples, save_dir=save_path_generated)
     else:
         print(f"Generated images already exist at: {save_path_generated}, skipping generation step.")
 
-    # Compute FID and IS using torch-fidelity
-    print()
-    print("Calculating FID and Inception Score...")
+    # Step 3: Compute FID and IS using torch-fidelity
+
+    print("\nComputing FID and Inception Score using torch-fidelity...")
     metrics = calculate_metrics(
         input1=save_path_real,
         input2=save_path_generated,
@@ -132,8 +144,18 @@ if __name__ == "__main__":
 
     """
     Findings:
-    - For celeba_32_5000 (real): IS = 3.2148 ± 0.0779
+    - For celeba_32_5000: IS (real) = 3.2148 ± 0.0779
         - For simple_unet32_celeba5000 (generated): 3.2148 ± 0.0779, FID = 34.6164
         - For unet32_celeba5000 (generated): TODO
         - For ho_unet32_celeba5000 (generated): TODO
+    - For digits_16_1797 (real): IS (real) = 1.7409 ± 0.0336
+        - Simple Unet with reduced architecture: down_channels = (32, 64, 128), up_channels = (128, 64, 32)
+                - After 100 epochs: 
+                    - IS: 1.7409 ± 0.0336, FID: 20.8106
+                - After 5000 epochs:
+                    - IS: 1.7409 ± 0.0336, FID: 6.0066
+                    - Nearest-neighbor pixel distances:
+                        - Gen->Real  NN distance (mean): 0.9834, median: 0.9644
+                        - Real->Real NN distance (mean): 1.4271, median: 1.3974
+                        - Ratio (gen->real / real->real): 0.6889
     """
